d5e1e5d861584027e0dcb1f761b3b4bc1e36c2a1
Mike Koenig <koenigm@seattleu.edu>
2020-10-16 18:16:34 -0700

    Added example GraphQuestions.cs and tests

---
+++ UnitTests/GraphQuestionsTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ConsoleApp2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class GraphQuestionsTests
+    {
+        [TestMethod]
+        public void fdasfa()
+        {
+            // Arrange
+
+            // Act
+            //var result = StringQuestions.FirstUniqueChar("leetcode");
+
+            // Assert
+            //Assert.AreEqual(0, result);
+        }
+
+    }
+}
